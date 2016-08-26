845c084fe845af5c4729f4f3353f629dd838a661
timidus <timidus@sina.com>
2016-08-26 14:17:18 +0800

    Chinese translation for bootstrap-year-calendar
    汉语

---
+++ js/languages/bootstrap-year-calendar.zh-CN.js
@@ -0,0 +1,19 @@
+/**
+ * Chinese translation for bootstrap-year-calendar
+ * Paul DAVID-SIVELLE
+ * Based on
+ * Chinese translation for bootstrap-datepicker
+ * Timidus <timidus@sina.com>
+ */
+
+;(function($){
+    $.fn.calendar.dates['zh-CN'] = {
+        days: ["星期天", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期天"],
+        daysShort: ["周日", "周一", "周二", "周三", "周四", "周五", "周六", "周日"],
+        daysMin: ["七", "一", "二", "三", "四", "五", "六", "七"],
+        months: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
+        monthsShort: ["一", "二", "三", "四", "五", "六", "七", "八", "九", "十", "十一", "十二"],
+        weekShort: '周',
+        weekStart:0
+    };
+}(jQuery));
